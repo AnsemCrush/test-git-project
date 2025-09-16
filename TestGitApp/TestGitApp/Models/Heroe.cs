f4bcde5047a2b53df2840d290b722576e27fa524
SotirisPavlopoulos <spavlopoulos89@gmail.com>
2025-09-16 17:28:28 +0300

    Create Heroe.cs

---
+++ TestGitApp/TestGitApp/Models/Heroe.cs
@@ -0,0 +1,22 @@
+﻿namespace TestGitApp.Models
+{
+    public class Heroe
+    {
+
+       
+        public string? FullName { get; set; }
+      
+
+
+        public Heroe( string fullName)
+        {
+
+           
+            FullName = fullName;
+           
+        }
+        public Heroe() { }
+    }
+
+}
+}
