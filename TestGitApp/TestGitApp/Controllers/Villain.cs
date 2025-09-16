99357e38619463e6c838c2e6f2c0b6f57e6491a3
SotirisPavlopoulos <spavlopoulos89@gmail.com>
2025-09-16 18:02:17 +0300

    new Controller for villain entity
    Write base logic here ->

---
+++ TestGitApp/TestGitApp/Controllers/Villain.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestGitApp.Controllers
+{
+    public class Villain
+    {
+
+
+        [HttpGet]
+        public String GetAllVillains(String villainType)
+        {
+
+            return villainType;
+        }
+    }
+}
